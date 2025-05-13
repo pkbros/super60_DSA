18b6af83da5e6cfdb4d8bbace25a909c82546415
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2025-05-13 15:00:12 +0530

    2

--- Linked Lists/doubly_circular_LL.cpp
+++ Linked Lists/doubly_circular_LL.cpp
@@ -28,11 +28,48 @@ class DoublyCircularLinkedList{
             head = newNode;
             head->next = head;
             head->prev = head;
-            
+        }
+        else{
+            Node* tail = head->prev;
+            newNode->next = head;
+            newNode->prev = tail;
+            tail->next = newNode;
+            head->prev = newNode;
+            head = newNode;
         }
 
     }
-}
+
+    void insertAtTail(int value){
+        Node* newNode = new Node(value);
+        if(head == nullptr){
+            head = newNode;
+            head->next = head;
+            head->prev = head;
+        }
+        else{
+            Node* tail = head->prev;
+            tail->next = newNode;
+            newNode->prev = tail;
+            newNode->next = head;
+            head->prev = newNode;
+        }
+    }
+
+    void deleteHead(){
+        if(head == nullptr){
+            cout << "List is empty." << endl;
+            return;
+        }
+        if(head->next == head){
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node* tail = head->prev;
+        Node* temp = head
+    }
+};
 
 
 int main(){
